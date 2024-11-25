22e47c5fd2ed57789483c11f2182fdfdf7422473
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2024-11-25 17:04:05 +0100

    03-conta vocali complete

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,24 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const contaVocali = (stringa) =>  {
 
+    let counter = 0;
+
+    const vocali = ['a','e','i','o','u'];
+
+    for(let i = 0; i < stringa.length; i++) {
+        console.log(stringa[i]);
+        
+        if(vocali.includes(stringa[i].toLowerCase())) counter++;
+    }
+    
+    return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(contaVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
