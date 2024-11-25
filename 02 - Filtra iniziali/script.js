bbd6af430754470d161d95654b3e693bc4a0988a
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2024-11-25 16:19:25 +0100

    02-Filtra iniziali complete

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const inizialiFiltrate = (nomi, lettera) => {
+    const nomiFiltrati = [];
 
+    for(let i = 0; i < nomi.length; i++){
+        const iniziale = nomi[i][0];
+        if(iniziale === lettera){
+            nomiFiltrati.push(nomi[i])
+        }
+    } 
+    return nomiFiltrati;
+}   
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(inizialiFiltrate(names, 'A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
